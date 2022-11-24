95da1b6320852d98381ea6841519d1c85a2ab96d
Carolina Diazgranados <linacello@gmail.com>
2022-11-23 22:11:56 -0500

    created more tests and solved the challenge

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
@@ -8,7 +11,60 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+
+        ArrayList<String> doubleArray = new ArrayList<String>();
+        doubleArray.addAll(Arrays.asList(playList));
+        doubleArray.addAll(Arrays.asList(playList));
+        System.out.println(doubleArray);
+
+
+        int backwardCount = 0;
+
+        //find backward
+        for(int i = (startIndex + playList.length); i > 0; i--) {
+            if (doubleArray.get(i).equals(selection)) {
+                System.out.println("does this work?");
+                break;
+            }
+            backwardCount++;
+        }
+
+        int forwardCount = 0;
+        for(int i = startIndex; i < doubleArray.size(); i++) {
+
+            if (doubleArray.get(i).equals(selection)) {
+                break;
+            }
+            forwardCount++;
+        }
+
+        if (backwardCount < forwardCount) {
+            return backwardCount;
+        }
+        return forwardCount;
     }
+
+
+//public Integer selection(Integer startIndex, String selection) {
+//        int selectionIndex = 0; //3
+//
+//
+//        for (int i = 0; i < playList.length; i++) {
+//            if (playList[i].equals(selection)) {
+//                selectionIndex = i;
+//            }
+//        }
+//
+//        int forward = selectionIndex - startIndex;
+//        int backward = (startIndex + playList.length) - selectionIndex;
+//
+//        if (forward < backward) {
+//            return forward;
+//        }
+//
+//        return backward;
+//
+//}
+
 }
--- src/test/java/io/zipcoder/MusicTest.java
+++ src/test/java/io/zipcoder/MusicTest.java
@@ -8,6 +8,7 @@ public class MusicTest {
     @Test
     public void selectionTest1(){
 
+
         String[] playlist = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
         Music music = new Music(playlist);
         Integer startingIndex = 1;
@@ -20,6 +21,7 @@ public class MusicTest {
     @Test
     public void selectionTest2(){
 
+
         String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
         Music music = new Music(playlist);
         Integer startingIndex = 0;
@@ -28,4 +30,72 @@ public class MusicTest {
         Integer actual = music.selection(startingIndex, selection);
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void selectionTest3(){
+
+
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 3;
+        String selection = "dancinginthedark";
+        Integer expected = 1;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void selectionTest4(){
+
+
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "liveoak";
+        Integer expected = 1;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void selectionTest5(){
+
+
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak","wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 7;
+        String selection = "rio";
+        Integer expected = 2;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void selectionTest6(){
+
+
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak","wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "rio";
+        Integer expected = 0;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void selectionTest7(){
+
+
+        String[] playlist = {"dancinginthedark","rio","liveoak","liveoak","wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
+        Music music = new Music(playlist);
+        Integer startingIndex = 1;
+        String selection = "cecelia";
+        Integer expected = 2;
+        Integer actual = music.selection(startingIndex, selection);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
 }
